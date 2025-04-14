91b797294b6ffb1f5ac42b3f5ec56642f67266a2
thiennga52004 <thienlet1.str@gmail.com>
2025-04-14 19:48:10 +0700

    update

--- test.py
+++ test.py
@@ -5,21 +5,17 @@ import tensorflow as tf
 from tensorflow.python.keras.models import load_model
 
 
-#############################################
-
-frameWidth = 640  # CAMERA RESOLUTION
+frameWidth = 640
 frameHeight = 480
 brightness = 180
-threshold = 0.75  # PROBABLITY THRESHOLD
+threshold = 0.75
 font = cv2.FONT_HERSHEY_SIMPLEX
-##############################################
 
-# SETUP THE VIDEO CAMERA
 cap = cv2.VideoCapture(0)
 cap.set(3, frameWidth)
 cap.set(4, frameHeight)
 cap.set(10, brightness)
-# IMPORT THE TRANNIED MODEL
+
 model = tf.keras.models.load_model("traffic_sign_model.h5")  ## rb = READ BYTE
 
 
@@ -27,18 +23,15 @@ def grayscale(img):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     return img
 
-
 def equalize(img):
     img = cv2.equalizeHist(img)
     return img
-
 
 def preprocessing(img):
     img = grayscale(img)
     img = equalize(img)
     img = img / 255
     return img
-
 
 def getCalssName(classNo):
     if classNo == 0:
@@ -128,12 +121,9 @@ def getCalssName(classNo):
     elif classNo == 42:
         return 'End of no passing by vechiles over 3.5 metric tons'
 
-
 while True:
-    # READ IMAGE
     success, imgOrignal = cap.read()
 
-    # PROCESS IMAGE
     img = np.asarray(imgOrignal)
     img = cv2.resize(img, (32, 32))
     img = preprocessing(img)
@@ -141,7 +131,7 @@ while True:
     img = img.reshape(1, 32, 32, 1)
     cv2.putText(imgOrignal, "CLASS: ", (20, 35), font, 0.75, (7, 183, 101), 2, cv2.LINE_AA)
     cv2.putText(imgOrignal, "PROBABILITY: ", (20, 75), font, 0.75, (7, 183, 101), 2, cv2.LINE_AA)
-    # PREDICT IMAGE
+
     predictions = model.predict(img)
     classIndex = np.argmax(predictions)
     probabilityValue = np.amax(predictions)
